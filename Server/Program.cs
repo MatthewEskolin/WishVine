75c231b5235f4ee16650b29edc894e80e84aefa6
Matthew Eskolin <matteskolin@gmail.com>
2023-08-06 13:28:57 -0500

    fix build

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using WishVine.Server.AutoMapper;
 using WishVine.Server.Controllers;
+using WishVine.Server.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
