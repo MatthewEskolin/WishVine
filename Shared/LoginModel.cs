739bfca0a79681498daa76301a7d5219cd912241
Matthew Eskolin <matteskolin@gmail.com>
2023-10-25 20:16:00 -0500

    go

--- Shared/LoginModel.cs
+++ Shared/LoginModel.cs
@@ -10,11 +10,9 @@ namespace WishVine.Shared
 {
     public class LoginModel
     {
-        [Required]
-        public string UserName { get; set; }
+        [Required] public string UserName { get; set; } = null!;
 
-        [Required]
-        public string Password { get; set; }
+        [Required] public string Password { get; set; } = null!;
 
         public bool RememberMe { get; set; }
     }
